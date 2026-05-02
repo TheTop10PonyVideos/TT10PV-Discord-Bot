e9e133aa6de541bd27f105538d0e24178407dcdc
Brambles-c <gael.alejos05@gmail.com>
2026-05-01 17:12:47 -0700

    Channel list to not listen from

--- cogs/messages.py
+++ cogs/messages.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 from discord import Message
 from server_actions.annotations import get_video_data
-from config import target_guild_id
+from config import target_guild_id, ignore_channels
 from bot.whitelist_scheduling import schedule_whitelist
 import re, asyncio
 
@@ -12,11 +12,18 @@ platform_check = re.compile(r'https?://(?:[\w-]+\.)?(?:pony\.tube|youtube\.com|y
 class MessageListener(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, msg: Message):
-        if msg.author.bot or msg.guild is None or msg.guild.id != target_guild_id:
+        if (
+            msg.author.bot
+            or msg.guild is None
+            or msg.guild.id != target_guild_id
+            or msg.channel.id in ignore_channels
+        ):
             return
 
+        # Extract at most 3 video links from a message that are from supported platforms
         links = re.findall(platform_check, msg.content)[:3]
         
+        # Get the video data for each link by making validation requests to the form
         results = await asyncio.gather(
             *[get_video_data(link) for link in links],
             return_exceptions=True
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ server_api_url = os.getenv('API_ENDPOINT')
 server_auth_key = os.getenv('SERVER_AUTH_KEY')
 output_channel_id = int(os.getenv('OUTPUT_CHANNEL_ID'))
 target_guild_id = int(os.getenv('GUILD_ID'))
+ignore_channels = [int(channel_id) for channel_id in os.getenv('IGNORE_CHANNELS').split(',')]
 
 class Roles:
     MODERATOR = 749754741399224422
